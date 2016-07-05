40e2d408c08f7e7c23d3f84d1d790ec349603a35
feicien <ithcheng@gmail.com>
2016-07-05 17:29:57 +0800

    format code: use 4 spaces instead of tabs

--- sample/src/main/java/com/loveplusplus/update/sample/MainActivity.java
+++ sample/src/main/java/com/loveplusplus/update/sample/MainActivity.java
@@ -1,7 +1,6 @@
 package com.loveplusplus.update.sample;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,29 +12,29 @@ public class MainActivity extends ActionBarActivity {
 
     protected static final String APP_UPDATE_SERVER_URL = "http://192.168.205.33:8080/Hello/api/update";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		Button btn1 = (Button) findViewById(R.id.button1);
-		Button btn2 = (Button) findViewById(R.id.button2);
+        Button btn1 = (Button) findViewById(R.id.button1);
+        Button btn2 = (Button) findViewById(R.id.button2);
 
-		btn1.setOnClickListener(new OnClickListener() {
+        btn1.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				UpdateChecker.checkForDialog(MainActivity.this,APP_UPDATE_SERVER_URL);
-			}
-		});
-		btn2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UpdateChecker.checkForDialog(MainActivity.this, APP_UPDATE_SERVER_URL);
+            }
+        });
+        btn2.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				UpdateChecker.checkForNotification(MainActivity.this,APP_UPDATE_SERVER_URL);
-			}
-		});
+            @Override
+            public void onClick(View v) {
+                UpdateChecker.checkForNotification(MainActivity.this, APP_UPDATE_SERVER_URL);
+            }
+        });
 
-	}
+    }
 
 }
