a9e63a7ea090870a185d0ead44f9cb6fcea62207
程辉 <ithcheng@gmail.com>
2017-10-19 19:25:48 +0800

    adaptation android 7.0

---
+++ library/src/main/java/com/loveplusplus/update/UpdateApkFileProvider.java
@@ -0,0 +1,11 @@
+package com.loveplusplus.update;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * @author feicien (ithcheng@gmail.com)
+ * @since 2017-10-19 19:11
+ */
+
+public class UpdateApkFileProvider extends FileProvider {
+}
