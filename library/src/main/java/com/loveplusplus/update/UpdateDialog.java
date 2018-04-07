0a598620aa9f265916f5e44d991a710f6d9c7c20
程辉 <chengh@dxy.cn>
2018-04-07 17:22:48 +0800

    adapter Android 8.0 notification

--- library/src/main/java/com/loveplusplus/update/UpdateDialog.java
+++ library/src/main/java/com/loveplusplus/update/UpdateDialog.java
@@ -12,9 +12,9 @@ class UpdateDialog {
 
     static void show(final Context context, String content, final String downloadUrl) {
         if (isContextValid(context)) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(context);
-            builder.setTitle(R.string.android_auto_update_dialog_title);
-            builder.setMessage(Html.fromHtml(content))
+            new AlertDialog.Builder(context)
+                    .setTitle(R.string.android_auto_update_dialog_title)
+                    .setMessage(Html.fromHtml(content))
                     .setPositiveButton(R.string.android_auto_update_dialog_btn_download, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
                             goToDownload(context, downloadUrl);
@@ -23,12 +23,9 @@ class UpdateDialog {
                     .setNegativeButton(R.string.android_auto_update_dialog_btn_cancel, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
                         }
-                    });
-
-            AlertDialog dialog = builder.create();
-            //点击对话框外面,对话框不消失
-            dialog.setCanceledOnTouchOutside(false);
-            dialog.show();
+                    })
+                    .setCancelable(false)
+                    .show();
         }
     }
 
